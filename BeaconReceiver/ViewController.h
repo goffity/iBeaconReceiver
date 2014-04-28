7f623e587d29bbe00982984704df9b54e41c074a
Narongwate Sangsakul <goffity@gmail.com>
2014-04-29 01:19:13 +0700

    init.

--- BeaconReceiver/ViewController.h
+++ BeaconReceiver/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+@property (strong, nonatomic) CLBeaconRegion *myBeaconRegion;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 @end
--- BeaconReceiver/ViewController.m
+++ BeaconReceiver/ViewController.m
@@ -18,12 +18,52 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Initialize location manager and set ourselves as the delegate
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    
+    // Create a NSUUID with the same UUID as the broadcasting beacon
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"52362C17-76E1-404B-AFC6-95DE3B372CA1"];
+    
+    // Setup a new region with that UUID and same identifier as the broadcasting beacon
+    self.myBeaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                             identifier:@"com.goffity.test.ibeacon"];
+    
+    // Tell location manager to start monitoring for the beacon region
+    [self.locationManager startMonitoringForRegion:self.myBeaconRegion];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)locationManager:(CLLocationManager*)manager didEnterRegion:(CLRegion*)region
+{
+    [self.locationManager startRangingBeaconsInRegion:self.myBeaconRegion];
+}
+
+-(void)locationManager:(CLLocationManager*)manager didExitRegion:(CLRegion*)region
+{
+    [self.locationManager stopRangingBeaconsInRegion:self.myBeaconRegion];
+    self.statusLabel.text = @"No";
+}
+
+-(void)locationManager:(CLLocationManager*)manager
+       didRangeBeacons:(NSArray*)beacons
+              inRegion:(CLBeaconRegion*)region
+{
+    // Beacon found!
+    self.statusLabel.text = @"Beacon found!";
+    
+    CLBeacon *foundBeacon = [beacons firstObject];
+    
+    // You can retrieve the beacon data from its properties
+    //NSString *uuid = foundBeacon.proximityUUID.UUIDString;
+    //NSString *major = [NSString stringWithFormat:@"%@", foundBeacon.major];
+    //NSString *minor = [NSString stringWithFormat:@"%@", foundBeacon.minor];
 }
 
 @end
